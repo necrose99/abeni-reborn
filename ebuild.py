f3ff667770bf7611071047a651c3045234ae23a1
Marius Mauch <genone@genone.de>
2003-08-28 17:51:01 +0000

    header parsing implemented

--- ebuild.py
+++ ebuild.py
@@ -1,8 +1,12 @@
-import portage, string, re
+import portage, string, re, os
 
 settings = portage.config().environ()
 portdir = settings['PORTDIR']
 overlays = settings['PORTDIR_OVERLAY'].split(" ")
+
+# error codes
+ERR_FILE_NOT_FOUND  = 1
+ERR_SYNTAX 			= 2
 
 class eclass:
 	def __init__(self, name):
@@ -36,7 +40,9 @@ class ebuild:
 		self.variables = []
 		self.header = ""
 		self.statements = []
-		
+
+		self.content = ""
+				
 	# constructor to get the ebuild by filename
 	def __init__(self, filename):
 		self.__localinit__()
@@ -64,6 +70,8 @@ class ebuild:
 		else:
 			self.version = self.version[2]+"-"+self.version[3]
 
+		self.path = self.location+"/"+self.category+"/"+self.package+"/"+self.filename
+
 	# constructor to get the ebuild by location, category, package and version
 	def __init__(self, category, package, version, location=portdir):
 		self.__localinit__()
@@ -75,11 +83,36 @@ class ebuild:
 		self.package = package
 		self.version = version
 		self.filename = package+"-"+version+".ebuild"
+		self.path = self.location+"/"+self.category+"/"+self.package+"/"+self.filename
+		
+	# read and parse the ebuild
+	def readEbuild(self):
+		if not os.path.exists(self.path):
+			return ERR_FILE_NOT_FOUND
+		
+		# check for bash syntax errors
+		if os.system("/bin/bash -n " + self.path) != 0:
+			return ERR_SYNTAX
 
+		# read the complete content
+		myfile = open(self.path, "r")
+		self.content = myfile.readlines()
+		myfile.close()
+		
+		# now parse the whole thing
+		ln = 0	# linenumber
+		l = ""	# current line
+		pos = 1	# current element position
+		header_finished = False
+		for l in self.content:
+			ln += 1
+			if not header_finished and l[0] == "#":
+				self.header += l
+				continue
+			header_finished = True
+			
+				
 # test code
-sc = ebuild("net-mail", "sylpheed-claws", "0.9.4")
-print sc.location
-print sc.filename
-print sc.category
-print sc.package
-print sc.version
+sc = ebuild("net-mail", "sylpheed-claws", "0.9.4", "/usr/portage/local")
+sc.readEbuild()
+print sc.header
